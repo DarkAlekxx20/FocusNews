c491edb911d5b6ca18f109cba1ba573e43541ee2
DarkAlekxx20 <niko.beelik@hotmail.com>
2025-01-26 22:10:43 -0600

    Se agregan modelos necesarios para el proyecto tomando como base los modelos de la base de datos

---
+++ src/java/org/focusnews/model/Usuario.java
@@ -0,0 +1,69 @@
+package org.focusnews.model;
+
+/**
+ * @author DarkAlekxx20
+ */
+public class Usuario {
+
+    private int idUsuario;
+    private String nombreUsuario;
+    private String contrasenia;
+    private String correo;
+    private String rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String nombreUsuario, String contrasenia, String correo, String rol) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+        this.correo = correo;
+        this.rol = rol;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "idUsuario=" + idUsuario + ", nombreUsuario=" + nombreUsuario + ", contrasenia=" + contrasenia + ", correo=" + correo + ", rol=" + rol + '}';
+    }
+}
